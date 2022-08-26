6b5617856ac4d79f33ea2f981eb13688a000df4e
Ronit <pictronit007@gmail.com>
2022-08-26 12:10:20 -0700

    Refactor code

--- app.go
+++ app.go
@@ -11,31 +11,40 @@ var (
 )
 
 func main() {
-	handler := http.HandlerFunc(handleRequest)
-	http.Handle("/example", handler)
+	sign_up_handler := http.HandlerFunc(handle_sign_up_request)
+	login_handler := http.HandlerFunc(handle_login_request)
+	http.Handle("/login", login_handler)
+	http.Handle("/login", sign_up_handler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
+func handle_sign_up_request(w http.ResponseWriter, r *http.Request) {
 
-	u, p, ok := r.BasicAuth()
+	// Implement sign up function with db
+	w.WriteHeader(200)
+	return
+}
+
+func handle_login_request(w http.ResponseWriter, r *http.Request) {
+
+	user_name, user_password, ok := r.BasicAuth()
 	if !ok {
 		fmt.Println("Error parsing basic auth")
 		w.WriteHeader(401)
 		return
 	}
-	if u != username {
-		fmt.Printf("Username provided is correct: %s\n", u)
+	if user_name != username {
+		fmt.Printf("Username provided is correct: %s\n", user_name)
 		w.WriteHeader(401)
 		return
 	}
-	if p != password {
-		fmt.Printf("Password provided is correct: %s\n", u)
+	if user_password != password {
+		fmt.Printf("Password provided is correct: %s\n", user_password)
 		w.WriteHeader(401)
 		return
 	}
-	fmt.Printf("Username: %s\n", u)
-	fmt.Printf("Password: %s\n", p)
+	fmt.Printf("Username: %s\n", user_name)
+	fmt.Printf("Password: %s\n", user_password)
 	w.WriteHeader(200)
 	return
 }
